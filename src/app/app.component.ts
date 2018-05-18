06567affcbffeb8ef81438f3d7474271be9c9686
lordvictor6393 <lordvictor6393@gmail.com>
2018-05-18 19:54:15 -0400

    Adding needed files and scripts to populate DB

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { initializeApp, database} from 'firebase';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,22 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'app';
+
+  constructor() {
+    // Initialize Firebase
+    var config = {
+      apiKey: "AIzaSyDrf1esA4D4ni96BGacYKE0faK-skzwgdY",
+      authDomain: "fir-test-d73c1.firebaseapp.com",
+      databaseURL: "https://fir-test-d73c1.firebaseio.com",
+      projectId: "fir-test-d73c1",
+      storageBucket: "fir-test-d73c1.appspot.com",
+      messagingSenderId: "644622904998"
+    };
+    initializeApp(config);
+
+    var root = database().ref('messages/1');
+    root.on('value', function(snap) {
+      console.log(snap.val());
+    });
+  }
 }
---
+++ src/environments/firebase.config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+    apiKey: "AIzaSyDrf1esA4D4ni96BGacYKE0faK-skzwgdY",
+    authDomain: "fir-test-d73c1.firebaseapp.com",
+    databaseURL: "https://fir-test-d73c1.firebaseio.com",
+    projectId: "fir-test-d73c1",
+    storageBucket: "fir-test-d73c1.appspot.com",
+    messagingSenderId: "644622904998"
+};
